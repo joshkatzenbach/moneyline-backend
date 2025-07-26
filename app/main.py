0cc3d95986b266761286847ad3414c1421611776
Josh Katzenbach <joshkatzenbach@gmail.com>
2025-07-25 22:20:20 -0600

    Test endpoint

--- app/main.py
+++ app/main.py
@@ -7,3 +7,9 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     return {"message": "Moneyline backend is live!"}
+
+
+
+@app.get("/test")
+def test_endpoint():
+    return {"message": "This is a test endpoint!"}
